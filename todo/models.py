c9f6c04c531fce5d7ed1f871652a36496deabcf8
RIchard Manansala <richard232manansala@gmail.com>
2025-03-07 17:24:47 +0800

    Added Todo class

---
+++ todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Todo(models.Model):
+    id=models.AutoField(primary_key=True, auto_created=True)
+    title=models.CharField(max_length=25)
+    date=models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
