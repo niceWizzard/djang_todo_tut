b614b8f06e41b695200a51f441d4dc6229f6e0d7
RIchard Manansala <richard232manansala@gmail.com>
2025-03-07 18:53:05 +0800

    Added some basic login feature

--- users/views.py
+++ users/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def register_page(req : HttpRequest):
     return render(req, template_name='users/register.html')
 
 def login_page(req : HttpRequest):
-    return render(req, template_name='users/login.html')
+    form = AuthenticationForm()
+    if req.method == "POST":
+        form = AuthenticationForm(data=req.POST)
+        if form.is_valid():
+            return redirect('/')
+    return render(req, template_name='users/login.html', context={'form': form})
 
 def users_page(req: HttpRequest):
     return HttpResponse("HELLO")
