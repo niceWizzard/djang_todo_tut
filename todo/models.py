71dc9d9d7dd29b09407e7eb859a405a8a16839f3
RIchard Manansala <richard232manansala@gmail.com>
2025-03-07 20:22:44 +0800

    Added is_done to Todo

--- todo/models.py
+++ todo/models.py
@@ -7,6 +7,7 @@ class Todo(models.Model):
     id=models.AutoField(primary_key=True, auto_created=True)
     title=models.CharField(max_length=25)
     date=models.DateTimeField(auto_now_add=True)
+    is_done = models.BooleanField(default=False)
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     
 
