1bb690cdd32eb80212e13321a6d98fd847dff313
RIchard Manansala <richard232manansala@gmail.com>
2025-03-08 08:11:12 +0800

    Added create todo page

--- todo/urls.py
+++ todo/urls.py
@@ -17,13 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from todo.views import homepage, todo_page, todos_page
+from todo.views import homepage, todo_create, todo_page, todos_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('', homepage),
     path('todo/', todos_page),
+    path('todo/create/', todo_create),
     path('todo/<str:id>/', todo_page),
     path('users/', include('users.urls'))
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 
+from todo.forms import TodoForm
 from todo.models import Todo
 
 
@@ -8,6 +9,16 @@ def homepage(request : HttpRequest ) -> HttpResponse:
     # return HttpResponse("Hello World!")
     return render(request, 'home.html')
 
+def todo_create(req : HttpRequest):
+    form = TodoForm()
+    if req.method == "POST":
+        form = TodoForm(req.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.user = req.user
+            todo.save()
+            return redirect("/todo")
+    return render(req, 'create_todo.html', {'form': form})
 
 def todos_page(req : HttpRequest):
     todos = Todo.objects.all().filter(user=req.user)
